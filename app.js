2f1326d09e25c0b979a8c6ecb29b4a7009f77ead
avni33 <avni_aggarwal@epam.com>
2017-02-06 15:14:42 +0530

    herokudb changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // connect to MongoDB
-mongoose.connect('mongodb://userWE5:S86XB40LtIEdiCqE@mongodb/sampledb')
+mongoose.connect('mongodb://root:root@ds031661.mlab.com:31661/heroku_fd4tls00')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
